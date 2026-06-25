67967f995f0b8b7ed9b9e95323bdcd3cf73d0a38
eloyze <eloyze.oliveira06@aluno.ifce.edu.br>
2026-06-24 21:35:17 -0300

    detalhes finais

--- app.js
+++ app.js
@@ -44,12 +44,15 @@ app.set("view engine", "ejs");
 app.use(cors());
 
 
-app.use(express.json());
+app.use(express.json({
+    limit: "50mb"
+}));
 
 
 app.use(express.urlencoded({
 
-    extended: false
+    extended: false,
+    limit: "50mb"
 
 }));
 
